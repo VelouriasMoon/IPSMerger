d3c4d4277f13ff6c40dfb477e07b0feb21a23af9
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-01-06 23:01:08 -0400

    Net core rewrite

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Text;
 
 namespace IPSMerger
 {
@@ -16,30 +13,29 @@ namespace IPSMerger
                 string path = Directory.GetCurrentDirectory();
                 // Check for existing merged ips and delete it so it doesn't cause issues
                 if (File.Exists(path + Path.DirectorySeparatorChar + "merged.ips"))
-                {
                     File.Delete(path + Path.DirectorySeparatorChar + "merged.ips");
-                }
 
                 // Create new file stream for the new merged ips patch 
-                FileStream fs = File.Create(path + Path.DirectorySeparatorChar + "merged.ips");
-                byte[] info = new UTF8Encoding(true).GetBytes("PATCH");
-                fs.Write(info, 0, info.Length);
-                int num = args.Length;
-
-                // Load each one by one and copy the patch data to the new file
-                for (int i = 0; i < num; i++)
+                using (FileStream fs = File.Create(path + Path.DirectorySeparatorChar + "merged.ips"))
                 {
-                    Console.WriteLine("IPS Patch {0}: {1}", i, Convert.ToString(args[i]));
-                    byte[] file = File.ReadAllBytes(args[i]);
-                    byte[] patch = new byte[file.Length - 8];
-                    Buffer.BlockCopy(file, 5, patch, 0, patch.Length);
-                    fs.Write(patch, 0, patch.Length);
+                    byte[] info = new UTF8Encoding(true).GetBytes("PATCH");
+                    fs.Write(info, 0, info.Length);
+                    int num = args.Length;
+
+                    // Load each one by one and copy the patch data to the new file
+                    for (int i = 0; i < num; i++)
+                    {
+                        Console.WriteLine($"IPS Patch {i}: {args[i]}");
+                        byte[] file = File.ReadAllBytes(args[i]);
+                        byte[] patch = new byte[file.Length - 8];
+                        Buffer.BlockCopy(file, 5, patch, 0, patch.Length);
+                        fs.Write(patch, 0, patch.Length);
+                    }
+
+                    // Write the EOF bytes to finish
+                    byte[] end = new UTF8Encoding(true).GetBytes("EOF");
+                    fs.Write(end, 0, end.Length);
                 }
-
-                // Write the EOF bytes to finish
-                byte[] end = new UTF8Encoding(true).GetBytes("EOF");
-                fs.Write(end, 0, end.Length);
-
             }
             else
             {
